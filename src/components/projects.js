f6f96305aaa52d8753d4d4cf78b26d6c4a94de81
nishant <nishant@ccs.neu.edu>
2018-10-17 13:10:00 -0400

    added card for a project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,17 @@
 import React, { Component } from "react";
-import { Tabs, Tab } from "react-mdl";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  CardText,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
@@ -11,9 +23,25 @@ class Projects extends Component {
     /*an if statement for each project */
     if (this.state.activeTab === 0) {
       return (
-        <div>
-          <h1>This is Pokemon Project</h1>
-        </div>
+        <Card shadow={5} style={{ width: "750px", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "white",
+              height: "250px",
+              background:
+                "url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png) center / cover"
+            }}
+          />
+          <CardText>
+            <h2>This is my pokemon project</h2>
+          </CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="shared" />
+          </CardMenu>
+        </Card>
       );
     } else if (this.state.activeTab === 1) {
       return (
@@ -36,7 +64,13 @@ class Projects extends Component {
           <Tab>Pokemon Genetic algorithm</Tab>
           <Tab>Unity Handwriting recognition</Tab>
         </Tabs>
-        <section className="projects-grid">{this.toggleCategories()}</section>
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
